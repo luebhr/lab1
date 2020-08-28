99037f2f78747773c16ddaae0841b3881652cd9e
David John <djj@wfu.edu>
2020-08-27 21:25:10 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// This is a basic Java program template, put a short description of what this
+// program accomplishes here
+//
+// Your name
+// Date
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    
+}
